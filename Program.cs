01ea1db5c44af07f6f22f6eb4b509157aaa664fe
Yong <144024466+Yoyoclub@users.noreply.github.com>
2023-09-11 14:29:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class Win
+{
+    public bool Quest1;
+    public bool Quest2;
+    public bool Quest3;
+
+    public Win(bool quest1, bool quest2, bool quest3)
+    {
+        Quest1 = quest1;
+        Quest2 = quest2;
+        Quest3 = quest3;
+    }
+
+    public bool Condition()
+    {
+        if (Quest1 && Quest2 && Quest3)
+        {
+            Console.WriteLine("Game cleared");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
