2bf6e09c835071c6d04192a5a0cdc13a6d44ef70
JaySurj <144024297+JaySurj@users.noreply.github.com>
2023-09-12 06:49:46 +0200

    Add files via upload

--- Location.cs
+++ Location.cs
@@ -1,26 +1,69 @@
-﻿class Location
+﻿public class Location
 {
     public int ID;
-
     public string Name;
+    public Location LocationToNorth;
+    public Location LocationToEast;
+    public Location LocationToSouth;
+    public Location LocationToWest;
+    public Quest QuestAvailableHere;
+    public Monster MonsterLivingHere
 
-    public string Description;
-
-    public bool Changethis;
-
-    public bool Changethis2;
-
-    public Location (int id, string name, string Description, bool Changethis, bool Changethis2)
+    public Location(int id, string name, string description, int change1, int change2)
     {
-        this.ID = id;
-        this.Name = name;
-        this.Description = Description;
-        this.Changethis = Changethis;
-        this.Changethis2 = Changethis2;
+        ID = id;
+        Name = name;
     }
 
-    public static void QuestAvailableHere(int id)
-    {
 
+    public string Compass()
+    {
+        //Settings > Debug > Console: Collapse Identical Lines
+        string s = "From here you can go:\n";
+        if (LocationToNorth != null)
+        {
+            s += "    N\n    |\n";
+        }
+        if (LocationToWest != null)
+        {
+            s += "W---|";
+        }
+        else
+        {
+            s += "    |";
+        }
+        if (LocationToEast != null)
+        {
+            s += "---E";
+        }
+        s += "\n";
+        if (LocationToSouth != null)
+        {
+            s += "    |\n    S\n";
+        }
+        return s;
+    }
+
+    public string NextLocationOptions()
+    {
+        string s = "";
+        if (LocationToNorth != null)
+            s += "N";
+        if (LocationToEast != null)
+            s += "E";
+        if (LocationToSouth != null)
+            s += "S";
+        if (LocationToWest != null)
+            s += "W";
+        return s;
+    }
+
+    public Location GetLocationAt(string location)
+    {
+        if (location == "N") return LocationToNorth;
+        if (location == "E") return LocationToEast;
+        if (location == "S") return LocationToSouth;
+        if (location == "W") return LocationToWest;
+        return null;
     }
 }
