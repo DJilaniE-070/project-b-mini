ebcdd18f0c3c7c3864e0021e2d0dc5175a7d08e5
JaySurj <144024297+JaySurj@users.noreply.github.com>
2023-09-12 13:25:31 +0200

    Hier elk locatie een event 3 while loops en in elke case de while loop plaatsen

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
-﻿public class Program
+public class Program
 {
     public static void Main()
     {
+        //hier moet de intro van game story home
         Player player = new Player(World.Locations[0]);
         Console.WriteLine("Current location: " + player.Location.Name);
         Console.WriteLine(player.Location.Compass());
 
         bool wonGame = false;
 
-        while (player.Location.Name != "Goal")
+        while (wonGame == false)
         {
             Console.Write("Enter a direction (N/E/S/W) or type 'quit' to exit: ");
             string input = Console.ReadLine();
@@ -31,8 +32,47 @@
                     }
                     else
                     {
+                        // wat is beter story boven compass beneden of na elk input compass boven en story binnen
+                        // in de switch kun je in elk case een while loop zetten voor elk quest
+
                         Console.WriteLine("Current location: " + player.Location.Name);
                         Console.WriteLine(player.Location.Compass());
+                        switch (player.Location.Name)
+                        {
+                        case "Home":
+                            Console.WriteLine("write story home");
+                            break;
+                        case "Town square":
+                            Console.WriteLine("write story Town square");
+                            break;
+                        case "Alchemist's hut":
+                            Console.WriteLine("write story A H");
+                            break;
+                        case "Alchemist's garden":
+                            Console.WriteLine("write story A G");
+                            // hier de minigames voor quest in A G
+                            while (true)
+                            {
+                                break;
+                            }
+                            break;
+                        case "Farmhouse":
+                            Console.WriteLine("write story F H");
+                            break;
+                        case "Farmer's field":
+                            Console.WriteLine("write story F F");
+                            break;
+                        case "Guard post":
+                            Console.WriteLine("write story G P");
+                            break;
+                        case "Bridge":
+                            Console.WriteLine("write story B");
+                            break;
+                        case "Forest":
+                            Console.WriteLine("Congratulations! You have won the game!");
+                            wonGame = true;
+                            break;
+                        }
                     }
                 }
                 else
@@ -46,13 +86,7 @@
             }
         }
 
-        if (player.Location.Name == "Goal")
-        {
-            Console.WriteLine("Congratulations! You have won the game!");
-            wonGame = true;
-        }
-
-        if (!wonGame)
+        if (wonGame == true)
         {
             Console.WriteLine("Game over. You did not reach the goal.");
         }
